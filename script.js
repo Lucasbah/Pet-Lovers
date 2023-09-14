b005e91167735d99a9d80dd83d7df16a6d86aca2
LucasBah <lucaskeitaro@hotmail.com>
2023-09-14 17:51:39 +0000

    Modal Adicionado

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-Option
+const openModalButton = document.querySelector('#open-modal');
+const closeModalButton = document.querySelector('#close-modal');
+const fade = document.querySelector('#fade')
+const modal = document.querySelector('#modal')
+
+
+const toggleModal = () => {
+    modal.classList.toggle('hide');
+    fade.classList.toggle('hide');
+}
+
+[openModalButton, fade, closeModalButton].forEach((el)=> {
+    el.addEventListener('click', () => toggleModal())
+});
